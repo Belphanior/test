b6be247e7aa0c604491b96256057ad564184cac0
Andrey <gushtchin.av@gmail.com>
2017-02-22 03:47:16 +0300

    123

---
+++ core/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+class MyTestCase(TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_nothing(self):
+        self.assertTrue(False)
